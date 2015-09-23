55b3bec8b9ead7a833a161fa04c32f334914b223
Al Harrington <al.harrington@hp.com>
2015-09-22 19:15:00 -0700

    Adding copyright to remaining files
    Change-Id: Icf92b1c9260c1fd8f84a442fa48ef8057095245c
    Signed-off-by: Al Harrington <al.harrington@hp.com>

--- src/js/views/ViewInitMixin.js
+++ src/js/views/ViewInitMixin.js
@@ -1,7 +1,22 @@
 /*
+ (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may
+    not use this file except in compliance with the License. You may obtain
+    a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+    License for the specific language governing permissions and limitations
+    under the License.
+*/
+
+/*
  * Mixin that provides a hook before a routing transition is performed.
  * This will likely be changed after we upgrade to react-router v1.0.
- * @author Frank Wood
  */
 
 var SessionStore = require('SessionStore'),
