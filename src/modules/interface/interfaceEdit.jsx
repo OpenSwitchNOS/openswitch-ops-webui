398875aa0ff463c86038f6bdad4a28df0aef4a8b
Seetharam Pranihith Venuturupalli <svenuturupalli@hpe.com>
2016-04-19 15:23:17 -0700

    new: dev: added split interfaces
    Change-Id: I4bb80641ac8de68b110f4164aad405ebcf4f41ca
    Signed-off-by: Seetharam Pranihith Venuturupalli <svenuturupalli@hpe.com>

--- src/modules/interface/interfaceEdit.jsx
+++ src/modules/interface/interfaceEdit.jsx
@@ -27,6 +27,7 @@ import RadioButton from 'grommet/components/RadioButton';
 import Button from 'grommet/components/Button';
 import _ from 'lodash';
 import * as C from './interfaceConst.js';
+import ConfirmLayer from 'confirmLayer.jsx';
 
 
 class InterfaceEdit extends Component {
@@ -40,12 +41,8 @@ class InterfaceEdit extends Component {
 
   constructor(props) {
     super(props);
-    // TODO: do we really need to create IDs with lodash here?
-    this.fid = _.uniqueId('infEdit_');
-    this.state = { [C.USER_CFG]: {} };
+    this.state = { [C.USER_CFG]: {}};
   }
-
-  _id = s => `${this.fid}_${s}`;
 
   componentDidMount() {
     const p = this.props;
@@ -63,23 +60,217 @@ class InterfaceEdit extends Component {
   };
 
   _onOk = () => {
+    const laneSplit = this.state[C.USER_CFG][C.LANE_SPLIT];
+    if (laneSplit === 'split') {
+      this.setState({splitConfirm: true});
+    } else {
+      this.props.actions.interface.edit(this.state);
+      this.props.onClose();
+    }
+  };
+
+  _onConfirm = () => {
     this.props.actions.interface.edit(this.state);
     this.props.onClose();
   };
-
+ _onClose = () => {
+   this.props.onClose();
+ };
   _onChange = (setting) => {
-    const newUserCfg = { ...this.state[C.USER_CFG], ...setting };
+    const initUserCfg = this.props.interface.edit.interface[C.USER_CFG];
+    let newUserCfg = { ...this.state[C.USER_CFG], ...setting };
+    if (setting[C.LANE_SPLIT] === 'split') {
+      newUserCfg = {...initUserCfg};
+      newUserCfg[C.LANE_SPLIT] = 'split';
+    }
     this.setState({ [C.USER_CFG]: newUserCfg });
   };
 
-  render() {
+  _onSplitOk = () => {
+    const laneSplit = this.state[C.USER_CFG][C.LANE_SPLIT];
+    if (laneSplit === '') {
+      this.setState({unSplitConfirm: true});
+    }
+  };
+
+  _onConfirmUnSplit = () => {
+    this.props.actions.interface.unsplit();
+    this.props.onClose();
+  };
+
+  _mkSplit = () => {
+    const uc = this.state[C.USER_CFG];
+    const laneSplit = uc && uc[C.LANE_SPLIT];
+
+    return (
+      <div>
+        <b>{t('split')}</b>
+        <FormField>
+          <RadioButton
+              id="split"
+              label={t('split')}
+              checked={laneSplit === 'split'}
+              onChange={() => this._onChange({[C.LANE_SPLIT]: 'split'})} />
+          <RadioButton
+              id="unSplit"
+              label={t('unSplit')}
+              checked={laneSplit === ''}
+              onChange={() => this._onChange({[C.LANE_SPLIT]: ''})} />
+        </FormField>
+      </div>
+    );
+  };
+
+  _mkMainConfig = () => {
     const uc = this.state[C.USER_CFG];
     const admin = uc && uc[C.ADMIN];
     const duplex = uc && uc[C.DUPLEX];
     const autoNeg = uc && uc[C.AUTO_NEG];
     const flowCtrl = uc && uc[C.FLOW_CTRL];
+    const laneSplit = uc && uc[C.LANE_SPLIT];
+    const id = this.props.infId;
+    const inf = this.props.interface.interfaces[id];
+    const splitOptions = inf.canSplit;
+    const splitConfirm = !(this.state.splitConfirm) ? null :
+    <ConfirmLayer
+        onClose={() => this.setState({splitConfirm: false})}
+        onSubmit={this._onConfirm}
+        submitLabel={t('yes')}>
+        {t('areYouSureSplit')}
+      </ConfirmLayer>;
 
     return (
+      <div>
+      {splitConfirm}
+      <b>{t('configured')}</b>
+      <FormField>
+        <RadioButton
+            id="adminUp"
+            label={t('up')}
+            checked={admin === 'up'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.ADMIN]: 'up'})} />
+        <RadioButton
+            id="adminDown"
+            label={t('down')}
+            checked={admin === 'down'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.ADMIN]: 'down'})} />
+      </FormField>
+      <br/>
+      <b>{t('autoNeg')}</b>
+      <FormField>
+        <RadioButton
+            id="autoNegOn"
+            label={t('on')}
+            checked={autoNeg === 'on'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.AUTO_NEG]: 'on'})} />
+        <RadioButton
+            id="autoNegOff"
+            label={t('off')}
+            disabled={laneSplit==='split'}
+            checked={autoNeg === 'off'}
+            onChange={() => this._onChange({[C.AUTO_NEG]: 'off'})} />
+      </FormField>
+      <br/>
+      <b>{t('duplex')}</b>
+      <FormField>
+        <RadioButton
+            id="duplexFull"
+            label={t('full')}
+            checked={duplex === 'full'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.DUPLEX]: 'full'})} />
+        <RadioButton
+            id="duplexHalf"
+            label={t('half')}
+            checked={duplex === 'half'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.DUPLEX]: 'half'})} />
+      </FormField>
+      <br/>
+      <b>{t('flowControl')}</b>
+      <FormField>
+        <RadioButton
+            id="fcNone"
+            label={t('none')}
+            checked={flowCtrl === 'none'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.FLOW_CTRL]: 'none'})} />
+        <RadioButton
+            id="fcRx"
+            label={t('rx')}
+            checked={flowCtrl === 'rx'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.FLOW_CTRL]: 'rx'})} />
+        <RadioButton
+            id="fxTx"
+            label={t('tx')}
+            checked={flowCtrl === 'tx'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.FLOW_CTRL]: 'tx'})} />
+        <RadioButton
+            id="fcRxTx"
+            label={t('rxtx')}
+            checked={flowCtrl === 'rxtx'}
+            disabled={laneSplit==='split'}
+            onChange={() => this._onChange({[C.FLOW_CTRL]: 'rxtx'})} />
+      </FormField>
+      <br/>
+      {!splitOptions ? null : this._mkSplit()}
+      <br/>
+      <Footer pad={{vertical: 'medium'}}>
+        <Menu>
+          <Button
+              label={t('ok')}
+              primary
+              onClick={this._isDirty() ? this._onOk : null}/>
+        </Menu>
+      </Footer>
+      </div>
+    );
+  };
+
+  _mkSplitConfig = () => {
+    const unSplitConfirm = !(this.state.unSplitConfirm) ? null :
+    <ConfirmLayer
+        onClose={() => this.setState({unsplitConfirm: false})}
+        onSubmit={this._onConfirmUnSplit}
+        submitLabel={t('yes')}>
+        {t('areYouSureUnSplit')}
+      </ConfirmLayer>;
+
+    return (
+      <div>
+      {unSplitConfirm}
+        <hr/>
+        <b>{t('UnSplitInfCfg')}</b>
+        {this._mkSplit()}
+        <br/>
+        <Footer pad={{vertical: 'medium'}}>
+          <Menu>
+            <Button
+                label={t('ok')}
+                primary
+                onClick={this.state[C.USER_CFG][C.LANE_SPLIT] === 'split' ?
+                         null : this._onSplitOk}/>
+          </Menu>
+        </Footer>
+      </div>
+    );
+  };
+
+
+  render() {
+    const id = this.props.infId;
+    const inf = this.props.interface.interfaces[id];
+    const laneSplit = inf.cfgLaneSplit;
+    const cfg = (laneSplit === 'split') ?
+      this._mkSplitConfig() : this._mkMainConfig();
+
+    return (
+      <div>
       <Layer
           className="edit"
           onClose={this.props.onClose}
@@ -89,81 +280,10 @@ class InterfaceEdit extends Component {
         <Box pad="medium" className="flex1">
           <Title>{`${t('edit')} ${t('interface')}: ${this.props.infId}`}</Title>
           <br/>
-          <b>{t('configured')}</b>
-          <FormField>
-            <RadioButton
-                id={this._id('adminUp')}
-                label={t('up')}
-                checked={admin === 'up'}
-                onChange={() => this._onChange({[C.ADMIN]: 'up'})} />
-            <RadioButton
-                id={this._id('adminDown')}
-                label={t('down')}
-                checked={admin === 'down'}
-                onChange={() => this._onChange({[C.ADMIN]: 'down'})} />
-          </FormField>
-          <br/>
-          <b>{t('autoNeg')}</b>
-          <FormField>
-            <RadioButton
-                id={this._id('autoNegOn')}
-                label={t('on')}
-                checked={autoNeg === 'on'}
-                onChange={() => this._onChange({[C.AUTO_NEG]: 'on'})} />
-            <RadioButton
-                id={this._id('autoNegOff')}
-                label={t('off')}
-                checked={autoNeg === 'off'}
-                onChange={() => this._onChange({[C.AUTO_NEG]: 'off'})} />
-          </FormField>
-          <br/>
-          <b>{t('duplex')}</b>
-          <FormField>
-            <RadioButton
-                id={this._id('duplexFull')}
-                label={t('full')}
-                checked={duplex === 'full'}
-                onChange={() => this._onChange({[C.DUPLEX]: 'full'})} />
-            <RadioButton
-                id={this._id('duplexHalf')}
-                label={t('half')}
-                checked={duplex === 'half'}
-                onChange={() => this._onChange({[C.DUPLEX]: 'half'})} />
-          </FormField>
-          <br/>
-          <b>{t('flowControl')}</b>
-          <FormField>
-            <RadioButton
-                id={this._id('fcNone')}
-                label={t('none')}
-                checked={flowCtrl === 'none'}
-                onChange={() => this._onChange({[C.FLOW_CTRL]: 'none'})} />
-            <RadioButton
-                id={this._id('fcTx')}
-                label={t('rx')}
-                checked={flowCtrl === 'rx'}
-                onChange={() => this._onChange({[C.FLOW_CTRL]: 'rx'})} />
-            <RadioButton
-                id={this._id('fcRx')}
-                label={t('tx')}
-                checked={flowCtrl === 'tx'}
-                onChange={() => this._onChange({[C.FLOW_CTRL]: 'tx'})} />
-            <RadioButton
-                id={this._id('fcRxTx')}
-                label={t('rxtx')}
-                checked={flowCtrl === 'rxtx'}
-                onChange={() => this._onChange({[C.FLOW_CTRL]: 'rxtx'})} />
-          </FormField>
-          <Footer pad={{vertical: 'medium'}}>
-            <Menu>
-              <Button
-                  label={t('ok')}
-                  primary
-                  onClick={this._isDirty() ? this._onOk : null}/>
-            </Menu>
-          </Footer>
+          {cfg}
         </Box>
       </Layer>
+      </div>
     );
   }
 }
